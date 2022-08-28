6c558850d9e00bb5ebdd4c04e25be8c30402d07b
jaehyun.shim <jaehyun.shim@sebasi.co.kr>
2022-08-28 18:19:52 +0900

    making components reusable

--- src/App.js
+++ src/App.js
@@ -67,7 +67,14 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search search={searchTerm} onSearch={handleSearch} />
+      <InputWithLabel
+        id='search'
+        label='Search'
+        value={searchTerm}
+        onInputChange={handleSearch}
+      />
+
+      {/* <Search search={searchTerm} onSearch={handleSearch} /> */}
       <hr />
 
       <List list={searchedStories} />
@@ -76,30 +83,29 @@ const App = () => {
   )
 }
 
-const InputWithLabel = ({ id, label, value, onInputChange }) => (
+const InputWithLabel = ({ id, label, value, type = 'text', onInputChange }) => (
   <>
-  <label htmlFor={id}>{label}</label>
-  &nbsp;
-  <input 
-  id = {id}
-  type='text'
-  value={value}
-  onChange={onInputChange}
-  />
-  </>
-)
-
-const Search = ({ search, onSearch }) => (
-  <>
-    <label htmlFor='search'>Search: </label>
+    <label htmlFor={id}>{label}</label>
+    &nbsp;
     <input
-      id='search'
-      type='text'
-      value={search}
-      onChange={onSearch} />
+      id={id}
+      type={type}
+      value={value}
+      onChange={onInputChange}
+    />
   </>
 )
 
+// const Search = ({ search, onSearch }) => (
+//   <>
+//     <label htmlFor='search'>Search: </label>
+//     <input
+//       id='search'
+//       type='text'
+//       value={search}
+//       onChange={onSearch} />
+//   </>
+// )
 
 const List = ({ list }) =>
   list.map(item => <Item key={item.objectID} item={item} />)
